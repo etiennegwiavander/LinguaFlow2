abc5a55c93664a9eb27e001c6666b62d022b1f2d
Etienne <91129018+etiennegwiavander@users.noreply.github.com>
2025-06-07 23:15:34 +0100

    Updated index.ts

--- supabase/functions/cleanup-expired-accounts/index.ts
+++ supabase/functions/cleanup-expired-accounts/index.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.225.1/http/server.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
 import { createClient } from 'npm:@supabase/supabase-js@2'
 
 const corsHeaders = {
