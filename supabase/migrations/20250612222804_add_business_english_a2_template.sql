309fd3717fed6518492bebdb088f767265433d02
Etienne <91129018+etiennegwiavander@users.noreply.github.com>
2025-06-13 14:34:27 +0100

    Updated 20250612222804_add_business_english_a2_template.sql

---
+++ supabase/migrations/20250612222804_add_business_english_a2_template.sql
@@ -0,0 +1,7 @@
+-- Remove English for Kids C1 Lesson Template
+DELETE FROM lesson_templates
+WHERE name = 'English for Kids Lesson' AND category = 'English for Kids' AND level = 'c1';
+
+-- Remove English for Kids C2 Lesson Template
+DELETE FROM lesson_templates
+WHERE name = 'English for Kids Lesson' AND category = 'English for Kids' AND level = 'c2';
